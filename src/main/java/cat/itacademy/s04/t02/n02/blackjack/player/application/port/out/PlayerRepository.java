cc2436dbd51b232ec6c05ffb1e43381e6f657c0b
federicopratico <federico.pratico@icloud.com>
2026-04-28 16:39:02 +0200

    feature: add repository method to retrieve the rankin

--- src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/out/PlayerRepository.java
+++ src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/out/PlayerRepository.java
@@ -4,6 +4,7 @@ import cat.itacademy.s04.t02.n02.blackjack.player.domain.entity.Player;
 import cat.itacademy.s04.t02.n02.blackjack.player.domain.valueobject.identity.PlayerId;
 import cat.itacademy.s04.t02.n02.blackjack.player.domain.valueobject.identity.PlayerName;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -11,4 +12,5 @@ public interface PlayerRepository {
     Mono<Player> save(Player player);
     Mono<Player> findByName(PlayerName name);
     Mono<Player> findById(PlayerId playerId);
+    Flux<Player> findAll();
 }
