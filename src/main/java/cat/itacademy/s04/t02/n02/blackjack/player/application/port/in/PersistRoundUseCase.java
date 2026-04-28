dce51eb2c76449e997a45c9db34b437927d52be9
federicopratico <federico.pratico@icloud.com>
2026-04-28 16:21:01 +0200

    chore: change pakcage structure

--- src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/in/PersistRoundUseCase.java
+++ src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/in/PersistRoundUseCase.java
@@ -1,6 +1,6 @@
 package cat.itacademy.s04.t02.n02.blackjack.player.application.port.in;
 
-import cat.itacademy.s04.t02.n02.blackjack.player.application.command.PersistRoundCommand;
+import cat.itacademy.s04.t02.n02.blackjack.player.application.dto.command.PersistRoundCommand;
 import reactor.core.publisher.Mono;
 
 public interface PersistRoundUseCase {
--- src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/in/ReserveBetUseCase.java
+++ src/main/java/cat/itacademy/s04/t02/n02/blackjack/player/application/port/in/ReserveBetUseCase.java
@@ -1,6 +1,6 @@
 package cat.itacademy.s04.t02.n02.blackjack.player.application.port.in;
 
-import cat.itacademy.s04.t02.n02.blackjack.player.application.command.ReserveBetCommand;
+import cat.itacademy.s04.t02.n02.blackjack.player.application.dto.command.ReserveBetCommand;
 import reactor.core.publisher.Mono;
 
 public interface ReserveBetUseCase {
